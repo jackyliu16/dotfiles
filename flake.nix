7f11173dd65b31abf1b7b6bdb372e9413a1d3db1
jackyliu16 <18922251299@163.com>
2023-12-04 01:07:20 +0800

    record

--- flake.nix
+++ flake.nix
@@ -3,17 +3,15 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    # nixpkgs.url = "github:jackyliu16/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-unstable.url = "github:jackyliu16/nixpkgs";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+        # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
     nur.url = "github:nix-community/NUR";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # rust-overlay.url = "github:oxalica/rust-overlay";
@@ -57,6 +55,8 @@
         in import ./shell.nix { inherit pkgs; }
       );
 
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
       # Your custom packages and modifications, exported as overlays
       overlays = import ./overlays { inherit inputs; };
       # Reusable nixos modules you might want to export
@@ -73,16 +73,16 @@
           specialArgs = { inherit inputs outputs; };
           modules = [
             # > Our main nixos configuration file <
-            nur.nixosModules.nur
+            # nur.nixosModules.nur
             ./nixos/Dell/configuration.nix
             ({ pkgs, config, ... }: {
               # nixpkgs.overlays = [ rust-overlay.overlays.default ];
-              environment.systemPackages = [ 
-                # pkgs.rust-bin.nightly.latest.default 
-                # config.nur.repos.linyinfeng.matrix-wechat
-                # config.nur.repos.xddxdd.wine-wechat
-                config.nur.repos.linyinfeng.icalingua-plus-plus
-              ];
+              # environment.systemPackages = [ 
+              #   # pkgs.rust-bin.nightly.latest.default 
+              #   # config.nur.repos.linyinfeng.matrix-wechat
+              #   # config.nur.repos.xddxdd.wine-wechat
+              #   # config.nur.repos.linyinfeng.icalingua-plus-plus
+              # ];
             })
           ];
         };
