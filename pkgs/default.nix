7083bcd2a4aa2bfaabe8a6d3b71608b3e61a4cf0
jackyliu16 <18922251299@163.com>
2023-12-04 13:57:07 +0800

    FIX(pkgs): fix unreason failure on nixos build

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,27 +1,5 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
-
-{ pkgs ? (import ../nixpkgs.nix) { }
-, binutils ? (import pkgs.binutils {} )
-, ... }: rec {
+# You can build them using 'nix build .#example'
+pkgs: {
   # example = pkgs.callPackage ./example { };
-  mapPackages = f: with binutils; listToAttrs (
-    map
-    (name: {
-      inherit name;
-      value = f name;
-    })
-    (filter (v: v != null)
-      (attrValues
-        (mapAttrs (k: v: if v == "directory" && k != "_source" then k else null) (readDir ./.))))
-  );
-  packages = pkgs: mapPackages (name: pkgs.${name});
-  overlays = final: prev: mapPackages (name:
-    let
-      sources = (import ./_source/generated.nix) { inherit (final) fetchurl fetchgit fetchFromGitHub; };
-      packages = import ./${name};
-      args = binutils.intersectAttrs (binutils.functionArgs packages) { source = sources.${name}; };
-    in
-      final.callPackage packages args
-  );
 }
