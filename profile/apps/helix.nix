9b25832571900bd20e254e28d57cd86e532a098f
jackyliu16 <18922251299@163.com>
2024-01-21 04:00:08 +0800

    Copy(helix): ref on ryan4yin/nix-config

--- profile/apps/helix.nix
+++ profile/apps/helix.nix
@@ -1,6 +1,32 @@
 { pkgs, ... }: {
   programs.helix = {
     enable = true;
+    package = pkgs.helix;
+
+    settings = {
+      # theme = "";
+      editor = {
+        line-number = "relative";
+        cursorline = true;
+        color-modes = true;
+        lsp.display-messages = true;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        indent-guides.render = true;
+      };
+      key.normal = {
+        space = {
+          space = "file_picker";
+          w = ":w";
+          q = ":q";
+        };
+        esc = [ "collapse_selection" "keep_primary_selection" ];
+      };
+    };
+    
     # extraPackages = [];
     languages = with pkgs; {
       language-server.rust-analyzer.config.check.command = "clippy";
@@ -59,6 +85,5 @@
     };
 
     # themes = {};
-    # settings = {};
   };
 }
