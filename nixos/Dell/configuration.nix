bc0383d212c76f020ae9a1833a3a84319e9982ff
jackyliu16 <18922251299@163.com>
2024-01-26 07:00:55 +0800

    nixos(Dell): update efiSysMountPoint

--- nixos/Dell/configuration.nix
+++ nixos/Dell/configuration.nix
@@ -1,12 +1,11 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
+{ inputs
+, outputs
+, lib
+, config
+, pkgs
+, ...
 }: {
   # You can import other NixOS modules here
   imports = [
@@ -50,18 +49,18 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
+  nix.nixPath = [ "/etc/nix/path" ];
   environment.etc =
     lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
+      (name: value: {
+        name = "nix/path/${name}";
+        value.source = value.flake;
+      })
+      config.nix.registry;
 
   nix.settings = {
     # Enable flakes and new 'nix' command
@@ -70,7 +69,7 @@
     auto-optimise-store = true;
     substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
     trusted-substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
-    trusted-users = [ "jacky"  ];
+    trusted-users = [ "jacky" ];
   };
 
   nix.gc = {
@@ -93,6 +92,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   users.users = {
     jacky = {
@@ -121,10 +121,11 @@
   #     PasswordAuthentication = false;
   #   };
   # };
-    
+
   time.timeZone = "Asia/Shanghai";
 
-  services.xserver = { # X11 windowing services
+  services.xserver = {
+    # X11 windowing services
     enable = true;
     # Key-mapping
     layout = "cn";
