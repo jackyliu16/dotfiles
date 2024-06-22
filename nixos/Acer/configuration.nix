98698f5d671f5f85a8d6a95ce96e56e68e12277f
jackyliu16 <18922251299@163.com>
2024-06-22 10:15:45 +0800

    nixos(Acer): re-enable fonts

--- nixos/Acer/configuration.nix
+++ nixos/Acer/configuration.nix
@@ -20,7 +20,7 @@
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
     # ./applications.nix
-    # ../fonts.nix
+    ../fonts.nix
   ];
 
   nixpkgs = {
