44c73e3691aa0646134040235d208585b47948db
jackyliu16 <18922251299@163.com>
2024-06-25 00:51:52 +0800

    nixos(Acer): add nvidia support but not sure if correctfalse

--- nixos/Acer/hardware-configuration.nix
+++ nixos/Acer/hardware-configuration.nix
@@ -50,4 +50,17 @@
     enable = true;
     powerOnBoot = true;
   };
+    hardware.nvidia = {
+    # Modesetting is needed for most Wayland compositors
+    # We have it disabled since the Nvidia card is not primary GPU
+    # And it breaks graphics to have two GPUs with modesetting on both
+    modesetting.enable = false;
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = true;
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
