cf8d7ec00e2760518d9a57baaebbab3a5e7a5826
jackyliu16 <18922251299@163.com>
2024-07-09 18:00:05 +0800

    profile(comm): provide alias of nh operation

--- profile/comm.nix
+++ profile/comm.nix
@@ -31,6 +31,8 @@ in {
     # TODO: haven't check yet
     oss="sudo nixos-rebuild switch --flake ${repo_path}#${domain}";
     osb="sudo nixos-rebuild boot --flake ${repo_path}#${domain}";
+    nhos="nh os switch /home/${user}/.config/dotfiles/";
+    nhhs="nh home switch /home/${user}/.config/dotfiles/";
     send="curl -F 'c=@-' 'https://fars.ee/'";
   };
   programs.git = {
