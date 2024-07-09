d94c728464f456ac98f73f576eee7ba6960daafc
jackyliu16 <18922251299@163.com>
2024-07-09 18:04:07 +0800

    nixos(Acer:Kde): remove zanshin

--- nixos/Acer/kde.nix
+++ nixos/Acer/kde.nix
@@ -4,7 +4,6 @@
   environment.systemPackages = with pkgs.kdePackages; [
     partitionmanager  # Manage the disk devices, partitions and file systems
     filelight         # Quickly visualize your disk space usage
-    zanshin           # TEST ?
     yakuake           # Drop down terminal
   ];
 }
