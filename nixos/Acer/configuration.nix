4747f8eb951dde64e9236fc485d8bc4b62b410b6
jackyliu16 <18922251299@163.com>
2024-08-14 16:31:29 +0800

    nixos(Acer): update steam package

--- nixos/Acer/configuration.nix
+++ nixos/Acer/configuration.nix
@@ -238,6 +238,7 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    package = pkgs.unstable.steam;
   };
 
 
