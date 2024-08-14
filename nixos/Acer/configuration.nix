1ffeb1b82f9e276bb0a3b5a79979ac38b9f7728b
jackyliu16 <18922251299@163.com>
2024-08-14 16:31:20 +0800

    nixos(Acer): update hosts

--- nixos/Acer/configuration.nix
+++ nixos/Acer/configuration.nix
@@ -90,7 +90,7 @@
     # networking.wireless.enable = true;
     extraHosts = builtins.readFile (builtins.toString (pkgs.fetchurl {
       url = "https://raw.hellogithub.com/hosts";
-      hash = "sha256-o6heuzT8FAsUZi+ueIi+245k1gDr3+MNlpH1QzhETtE=";
+      hash = "sha256-Dz7i+CfxnAKUnvNfwNgU8mjVvCjx9lI6WKwS+oASQNg=";
     }));
   };
 
