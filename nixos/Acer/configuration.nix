11f2eb7c2fa060e763a8e774d4714d7b92e7b2dd
jackyliu16 <18922251299@163.com>
2024-10-25 19:08:32 +0800

    nixos(Acer): FIX: base on nix-ld readme change original options name

--- nixos/Acer/configuration.nix
+++ nixos/Acer/configuration.nix
@@ -258,11 +258,11 @@
   };
 
   # https://nix.dev/guides/faq#how-to-run-non-nix-executables
-  # programs.nix-ld.enable = true;
-  # programs.nix-ld.libraries = with pkgs; [
-  #  Add any missing dynamic libraries for unpackaged programs
-  #  here, NOT in environment.systemPackages
-  # ];
+  programs.nix-ld.dev.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
 
   programs.nh = {
     enable = true;
