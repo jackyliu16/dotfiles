8672c39ee3b8dbfc67cd4156b2bc7bf8fdddda51
jackyliu16 <18922251299@163.com>
2024-11-12 18:46:06 +0800

    chore(flake): use nur in Acer

--- nixos/Acer/configuration.nix
+++ nixos/Acer/configuration.nix
@@ -39,6 +39,7 @@
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
 
+      inputs.nur.overlay
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
